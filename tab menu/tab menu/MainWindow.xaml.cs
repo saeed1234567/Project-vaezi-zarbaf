786451279c00b78889ee1ecd0f824604e51766b3
Ali Zarbaf <zarbafali@gmail.com>
2019-05-15 12:14:26 -0700

    tab menu

---
+++ tab menu/tab menu/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tab_menu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+
+            GridCurser.Margin = new Thickness(40 + (200 * index), 0, 0, 0);
+
+            switch(index)
+            {
+                case 0:
+                    his_page.Background = Brushes.Aquamarine;
+                    break;
+                case 1:
+                    his_page.Background = Brushes.Beige;
+                    break;
+                case 2:
+                    his_page.Background = Brushes.Azure;
+                    break;
+                case 3:
+                    his_page.Background = Brushes.Black;
+                    break;
+                case 4:
+                    his_page.Background = Brushes.Bisque;
+                    break;
+
+
+            }
+        }
+    }
+}
